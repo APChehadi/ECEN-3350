0a94af9f740b215565a8f8c197a76c448267e7aa
E-Wawrzynek <57082811+E-Wawrzynek@users.noreply.github.com>
2020-03-25 17:42:46 -0600

    update

--- button.s
+++ button.s
@@ -4,22 +4,25 @@
 	.global _start
 _start: 
 	movia   r16, HEX1 		# Address of HEX3...HEX0 Displays
-	movia	r20, HEX2		# Address of HEX5...HEx4 Displays
 	movia   r17, PUSHBUTTONS # Address of pushbuttons
 	movia	r18, HEX_bits_1
 	movia	r19, HEX_bits_2
 
 SETUP:
-	ldwio	r4, 0(r18)		#load pattern for HEX Displays
-	ldwio	r4, 0(r19)
+	ldwio	r4, 0(r18)	#load pattern for HEX Displays
+	ldwio	r8, 0(r19)
+	ldwio	r5, 0(r17)	#input from pushbuttons
+	beq     r5, r0, NO_BUTTON
+	br		BUTTON
 	
 CHECK:
 	ldwio	r5, 0(r17)	#input from pushbuttons
 	beq     r5, r0, NO_BUTTON
-
+	br		BUTTON
+	
 BUTTON:
-	stwio	r4, 0(r16) #store HEX3...HEX0
-	roli	r4, r4, 8
+	stwio	r8, 0(r16) #store HEX3...HEX0
+	roli	r8, r8, -8
 	movia	r7, 500000
 
 DELAY2:
@@ -30,7 +33,7 @@ DELAY2:
 NO_BUTTON:
 	stwio	r4, 0(r16)	#store HEX3...HEX0
 	roli	r4, r4, 8
-	movia	r7, 5000000	#delay counter
+	movia	r7, 500000	#delay counter
 
 DELAY:
 	subi	r7, r7, 1
